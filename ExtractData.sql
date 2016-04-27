c097284122c7e483c01faca6ab8ec55167eefcc5
Taso87 <stefanotasin@gmail.com>
2016-04-27 10:03:10 +0200

    Create ExtractData.sql

---
+++ ExtractData.sql
@@ -0,0 +1,26 @@
+-------------------------------------------------------------------------------------------------------
+-- this query select data from serie_temporali table, by: 
+--    - type of dataseries (rainfall = 2, temperature = 6, relative humidity = 8, radiation = 13)
+--    - basin mask with a buffer (the buffer is on station for convenience) 
+--    - time (convert to unix epoch)
+-- the time is converted to human date
+-- the data values are hourly mediated 
+-------------------------------------------------------------------------------------------------------
+
+SELECT m.punti_monitoraggio_id,s.metadati_id,strftime('%Y-%m-%d %H', s.dataora / 1000, 'unixepoch') || ":00" as dataora,avg(s.valore) as valore,s.affidabilita
+FROM serie_temporali s,metadati m
+WHERE s.metadati_id IN (
+	SELECT m.id FROM metadati m
+	WHERE m.id IN (
+		SELECT m.id FROM metadati m 
+		WHERE m.tipologia_serie_temporali_id=8
+		AND m.punti_monitoraggio_id IN (
+			SELECT p.id FROM basin_mask bm, punti_monitoraggio p 
+			WHERE ST_Intersects(bm.the_geom, ST_Buffer(p.the_geom, 5000))
+		)
+	)
+)
+AND s.dataora>=strftime('%s','2003-01-01 00:00:00')*1000
+AND s.dataora<strftime('%s','2014-01-01 00:00:00')*1000
+AND s.metadati_id=m.id
+GROUP BY s.metadati_id, strftime('%Y-%m-%d %H', s.dataora / 1000, 'unixepoch')
